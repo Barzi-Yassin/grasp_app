bfcf824d8fac18344413a386e0ce457da3fb7c3d
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-07-25 16:52:23 +0300

    adding the end-drawer to other screens + cleaning end-drawer codes

--- lib/src/screens/screen_subject.dart
+++ lib/src/screens/screen_subject.dart
@@ -1,138 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:grasp_app/src/data/datalist_subject.dart';
 import 'package:grasp_app/src/methods/functions.dart';
-import 'package:grasp_app/src/routes/route_screens.dart';
 import 'package:grasp_app/src/widgets/end_drawer/end_drawer.dart';
 import 'package:grasp_app/src/widgets/subject_records.dart';
 
 class ScreenSubjects extends StatelessWidget {
   const ScreenSubjects({Key? key}) : super(key: key);
 
-  final Color _enddrawerHeaderStuffLineColor = Colors.teal;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      endDrawer: SafeArea(
-        child: Drawer(
-          backgroundColor: Colors.transparent,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-              ),
-              gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.centerLeft,
-                colors: [
-                  Colors.cyan.shade300,
-                  Colors.grey.shade400,
-                ],
-              ),
-            ),
-            child: Column(
-              children: [
-                // end-drawer header
-                Container(
-                  padding: const EdgeInsets.only(left: 20, top: 8, bottom: 2),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    // color: Colors.blue.shade300,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      // bottomRight: Radius.circular(20),
-                    ),
-                  ),
-                  // end-drawer all items in a row
-                  child: Row(
-                    children: [
-                      // end-drawer (text + divider)
-                      Expanded(
-                        flex: 5,
-                        child: SizedBox(
-                          height: 60,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            verticalDirection: VerticalDirection.down,
-                            children: [
-                              Container(
-                                alignment: Alignment.topLeft,
-                                padding:
-                                    const EdgeInsets.only(left: 10, bottom: 2),
-                                child: Text(
-                                  'Barzy Yasin',
-                                  textAlign: TextAlign.left,
-                                  maxLines: 1,
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: _enddrawerHeaderStuffLineColor,
-                                    fontWeight: FontWeight.w500,
-                                    letterSpacing: 0.5,
-                                  ),
-                                ),
-                              ),
-                              Divider(
-                                color: _enddrawerHeaderStuffLineColor,
-                                thickness: 1.9,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      // end-drawer (image)
-                      Expanded(
-                        flex: 2,
-                        child: Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border:
-                                Border.all(color: _enddrawerHeaderStuffLineColor, width: 2.5),
-                          ),
-                          alignment: Alignment.center,
-                          child: const CircleAvatar(
-                            backgroundImage: AssetImage('assets/images/3.jpeg'),
-                            radius: 26,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    // color: Colors.yellow,
-                    padding: const EdgeInsets.only(
-                        left: 15, top: 20, right: 35, bottom: 5),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Column(
-                          children: const [
-                            EndDrawer(enddrawerRecordId: 1, enddrawerRecordTitle: "My profile", enddrawerRecordRoutePath: RouteScreens.routeMyProfile ),
-                            SizedBox(height: 18),
-                            EndDrawer(enddrawerRecordId: 2, enddrawerRecordTitle: "Stars", enddrawerRecordRoutePath: RouteScreens.routeFilterStars ),
-                            EndDrawer(enddrawerRecordId: 3, enddrawerRecordTitle: "Favorites", enddrawerRecordRoutePath: RouteScreens.routeFilterFavorites ),
-                            EndDrawer(enddrawerRecordId: 4, enddrawerRecordTitle: "Importants", enddrawerRecordRoutePath: RouteScreens.routeFilterImportants ),
-                            EndDrawer(enddrawerRecordId: 5, enddrawerRecordTitle: "Archived Grasps", enddrawerRecordRoutePath: RouteScreens.routeFilterArchived ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            EndDrawer(enddrawerRecordId: 6, enddrawerRecordTitle: "Grasp guidance", enddrawerRecordRoutePath: RouteScreens.routeGraspGuidance ),
-                            EndDrawer(enddrawerRecordId: 7, enddrawerRecordTitle: "Exit", enddrawerRecordRoutePath: RouteScreens.routeInit ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
+      endDrawer: const SafeArea(
+        child: EndDrawer(),
       ),
       appBar: AppBar(
         backgroundColor: Colors.cyan.shade700,
@@ -177,3 +57,4 @@ class ScreenSubjects extends StatelessWidget {
     );
   }
 }
+
