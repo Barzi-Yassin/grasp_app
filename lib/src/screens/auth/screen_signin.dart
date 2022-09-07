84e32ef5b5f3a1353a4f2c3cc18cf189ef421770
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-09-07 19:33:01 +0300

    adding loading to signin screen

--- lib/src/screens/auth/screen_signin.dart
+++ lib/src/screens/auth/screen_signin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:grasp_app/src/reusable_codes/functions/loadings/loading_indicator.dart';
 import 'package:grasp_app/src/reusable_codes/widgets/auth_state_widgets.dart/auth_states.dart';
 import 'package:grasp_app/src/reusable_codes/functions/functions.dart';
 import 'package:grasp_app/src/screens/auth/screen_signup.dart';
@@ -15,11 +16,11 @@ class ScreenSignin extends StatefulWidget {
 
 class _ScreenSigninState extends State<ScreenSignin> {
   final TextEditingController controllerSigninEmail = TextEditingController();
-
   final TextEditingController controllerSigninPassword =
       TextEditingController();
 
-  ServiceAuth serviceAuth = ServiceAuth();
+  final ServiceAuth serviceAuth = ServiceAuth();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -28,79 +29,86 @@ class _ScreenSigninState extends State<ScreenSignin> {
       body: Container(
         decoration: backgroundGradientCyan(),
         padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          clipBehavior: Clip.hardEdge,
-          scrollDirection: Axis.vertical,
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.manual,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 100),
-              customeTextAuthHeader(theData: '• sign in •'),
-              const SizedBox(height: 100),
-              Form(
+        child: isLoading == true
+            ? loadingIndicator()
+            : SingleChildScrollView(
+                clipBehavior: Clip.hardEdge,
+                scrollDirection: Axis.vertical,
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.manual,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    // email
-                    InputEmail(theControllerEmail: controllerSigninEmail),
+                    const SizedBox(height: 100),
+                    customeTextAuthHeader(theData: '• sign in •'),
+                    const SizedBox(height: 100),
+                    Form(
+                      child: Column(
+                        children: [
+                          // email
+                          InputEmail(theControllerEmail: controllerSigninEmail),
 
-                    const SizedBox(height: 20),
+                          const SizedBox(height: 20),
 
-                    // password
-                    InputPassword(
-                        theControllerPassword: controllerSigninPassword),
+                          // password
+                          InputPassword(
+                              theControllerPassword: controllerSigninPassword),
 
-                    const SizedBox(height: 20),
+                          const SizedBox(height: 20),
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (controllerSigninEmail.text.isNotEmpty &&
+                            controllerSigninPassword.text.isNotEmpty) {
+                          setState(() => isLoading = true);
+                          debugPrint(
+                              'controllerSigninEmail= <${controllerSigninEmail.text}>');
+                          debugPrint(
+                              'controllerSigninPassword= <${controllerSigninPassword.text}>');
+                          await serviceAuth
+                              .signInUserWithEmailAndPassword(
+                            signInemail: controllerSigninEmail.text.trim(),
+                            signInpass: controllerSigninPassword.text,
+                          )
+                              .then(
+                            (credential) {
+                              if (credential != null) {
+                                debugPrint(
+                                  'controllerSigninEmail= <${controllerSigninEmail.text}>',
+                                );
+                                setState(() => isLoading = false);
+                                Get.offAll(
+                                  ScreenSubjects(
+                                    theUser: credential.user!,
+                                  ),
+                                );
+                              }
+                              setState(() => isLoading = false);
+                            },
+                          );
+                        } else {
+                          debugPrint('one field or more might be empty !');
+                          Get.snackbar(
+                              'error', 'one field or more might be empty !');
+                        }
+                      },
+                      child: const Text('Sign In'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Get.offAll(() => const ScreenSignup());
+                      },
+                      child: customeText(
+                        theData: '\nDon\'t Have Account? \nSign up here',
+                        theTextAlign: TextAlign.center,
+                      ),
+                    )
                   ],
                 ),
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  if (controllerSigninEmail.text.isNotEmpty &&
-                      controllerSigninPassword.text.isNotEmpty) {
-                    debugPrint(
-                        'controllerSigninEmail= <${controllerSigninEmail.text}>');
-                    debugPrint(
-                        'controllerSigninPassword= <${controllerSigninPassword.text}>');
-                    await serviceAuth
-                        .signInUserWithEmailAndPassword(
-                      signInemail: controllerSigninEmail.text.trim(),
-                      signInpass: controllerSigninPassword.text,
-                    )
-                        .then(
-                      (credential) {
-                        if (credential != null) {
-                          debugPrint(
-                            'controllerSigninEmail= <${controllerSigninEmail.text}>',
-                          );
-                          Get.offAll(
-                            ScreenSubjects(
-                              theUser: credential.user!,
-                            ),
-                          );
-                        }
-                      },
-                    );
-                  } else {
-                    debugPrint('one field or more might be empty !');
-                    Get.snackbar('error', 'one field or more might be empty !');
-                  }
-                },
-                child: const Text('Sign In'),
-              ),
-              TextButton(
-                onPressed: () {
-                  Get.offAll(() => const ScreenSignup());
-                },
-                child: customeText(
-                  theData: '\nDon\'t Have Account? \nSign up here',
-                  theTextAlign: TextAlign.center,
-                ),
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
