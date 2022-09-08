be772b904380c00b258fc23fe7eec4cf06f6d9f3
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-09-08 19:38:17 +0300

    updating GraspUserModel, adding infoUpdatedAt field

--- lib/src/models/grasp_user_model.dart
+++ lib/src/models/grasp_user_model.dart
@@ -7,14 +7,16 @@ class GraspUserModel {
   String email;
   String? name;
   String? imageUrl;
-  DateTime createdAt;
+  DateTime? accCreatedAt;
+  DateTime? infoUpdatedAt;
   GraspUserModel({
     required this.userInAppId,
     required this.uid,
     required this.email,
     this.name,
     this.imageUrl,
-    required this.createdAt,
+    this.accCreatedAt,
+    this.infoUpdatedAt,
   });
 
   GraspUserModel copyWith({
@@ -23,7 +25,8 @@ class GraspUserModel {
     String? email,
     String? name,
     String? imageUrl,
-    DateTime? createdAt,
+    DateTime? accCreatedAt,
+    DateTime? infoUpdatedAt,
   }) {
     return GraspUserModel(
       userInAppId: userInAppId ?? this.userInAppId,
@@ -31,7 +34,8 @@ class GraspUserModel {
       email: email ?? this.email,
       name: name ?? this.name,
       imageUrl: imageUrl ?? this.imageUrl,
-      createdAt: createdAt ?? this.createdAt,
+      accCreatedAt: accCreatedAt ?? this.accCreatedAt,
+      infoUpdatedAt: infoUpdatedAt ?? this.infoUpdatedAt,
     );
   }
 
@@ -42,7 +46,8 @@ class GraspUserModel {
       'email': email,
       'name': name,
       'imageUrl': imageUrl,
-      'createdAt': createdAt.millisecondsSinceEpoch,
+      'accCreatedAt': accCreatedAt!.millisecondsSinceEpoch,
+      'infoUpdatedAt': infoUpdatedAt!.millisecondsSinceEpoch,
     };
   }
 
@@ -53,39 +58,44 @@ class GraspUserModel {
       email: map['email'] as String,
       name: map['name'] != null ? map['name'] as String : null,
       imageUrl: map['imageUrl'] != null ? map['imageUrl'] as String : null,
-      createdAt: DateTime.fromMillisecondsSinceEpoch(map['createdAt'] as int),
+      accCreatedAt:
+          DateTime.fromMillisecondsSinceEpoch(map['accCreatedAt'] as int),
+      infoUpdatedAt:
+          DateTime.fromMillisecondsSinceEpoch(map['infoUpdatedAt'] as int),
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory GraspUserModel.fromJson(String source) => GraspUserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory GraspUserModel.fromJson(String source) =>
+      GraspUserModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() {
-    return 'GraspUserModel(userInAppId: $userInAppId, uid: $uid, email: $email, name: $name, imageUrl: $imageUrl, createdAt: $createdAt)';
+    return 'GraspUserModel(userInAppId: $userInAppId, uid: $uid, email: $email, name: $name, imageUrl: $imageUrl, accCreatedAt: $accCreatedAt, infoUpdatedAt: $infoUpdatedAt)';
   }
 
   @override
   bool operator ==(covariant GraspUserModel other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.userInAppId == userInAppId &&
-      other.uid == uid &&
-      other.email == email &&
-      other.name == name &&
-      other.imageUrl == imageUrl &&
-      other.createdAt == createdAt;
+
+    return other.userInAppId == userInAppId &&
+        other.uid == uid &&
+        other.email == email &&
+        other.name == name &&
+        other.imageUrl == imageUrl &&
+        other.accCreatedAt == accCreatedAt &&
+        other.infoUpdatedAt == infoUpdatedAt;
   }
 
   @override
   int get hashCode {
     return userInAppId.hashCode ^
-      uid.hashCode ^
-      email.hashCode ^
-      name.hashCode ^
-      imageUrl.hashCode ^
-      createdAt.hashCode;
+        uid.hashCode ^
+        email.hashCode ^
+        name.hashCode ^
+        imageUrl.hashCode ^
+        accCreatedAt.hashCode ^
+        infoUpdatedAt.hashCode;
   }
 }
