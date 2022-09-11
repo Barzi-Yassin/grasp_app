fd53f2119cd60edf7f7da6146d27118257d88a2c
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-09-11 20:41:11 +0300

    simple screen messages structure

--- lib/src/screens/main_screens/screen_messages.dart
+++ lib/src/screens/main_screens/screen_messages.dart
@@ -1,10 +1,9 @@
 import 'package:badges/badges.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:grasp_app/src/reusable_codes/functions/functions.dart';
 
-class ScreenMessages extends StatelessWidget {
+class ScreenMessages extends StatefulWidget {
   const ScreenMessages({
     super.key,
     required this.theUser,
@@ -17,15 +16,30 @@ class ScreenMessages extends StatelessWidget {
   final String theFileSubjectName;
 
   @override
+  State<ScreenMessages> createState() => _ScreenMessagesState();
+}
+
+class _ScreenMessagesState extends State<ScreenMessages> {
+  final TextEditingController controllerMessage = TextEditingController();
+
+  // var focusNode = FocusNode();
+  // @override
+  // void initState() {
+  //   focusNode.addListener(() {
+  //     debugPrint(focusNode.hasFocus.toString());
+  //   });
+  //   super.initState();
+  // }
+
+  @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double sss = screenWidth - 15;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.cyan.shade700,
           centerTitle: true,
-          title: Text('$theFileSubjectName / $theFileName'),
+          title: Text('${widget.theFileSubjectName} > ${widget.theFileName}'),
         ),
         body: Container(
           // padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
@@ -34,7 +48,8 @@ class ScreenMessages extends StatelessWidget {
           decoration: backgroundGradientCyan(),
           child: Badge(
             // toAnimate: false,
-            animationType: BadgeAnimationType.scale,
+            animationType: BadgeAnimationType.fade,
+            animationDuration: const Duration(milliseconds: 700),
             // padding: const EdgeInsets.all(10),
             // gradient: LinearGradient(
             //   begin: Alignment.topLeft,
@@ -50,13 +65,23 @@ class ScreenMessages extends StatelessWidget {
             badgeColor: Colors.transparent,
             showBadge: true,
             badgeContent: Container(
-              height: 60,
+              height: 50,
               width: screenWidth - 20,
               // child: customeText(theData: 'fffffff'),
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(50)
-              ),
+                  // color: Colors.cyan,
+                  borderRadius: BorderRadius.circular(50),
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.grey.shade300,
+                      Colors.grey.shade100,
+                      Colors.white,
+                      // Colors.grey.shade100,
+                      // Colors.grey.shade300,
+                    ],
+                  )),
               child: customeIconButton(
                   theOnPressed: () {
                     debugPrint(screenWidth.toString());
@@ -65,12 +90,80 @@ class ScreenMessages extends StatelessWidget {
                   theIcon: Icons.add),
             ),
             child: Container(
-              margin: const EdgeInsets.only(left: 5, right: 5, top: 45, bottom: 5),
+              margin:
+                  const EdgeInsets.only(left: 5, right: 5, top: 40, bottom: 5),
+              padding: const EdgeInsets.only(top: 22.5),
+              width: double.infinity,
               // height: 200,
               // alignment: Alignment.topCenter,
               decoration: BoxDecoration(
-                // shape: BoxShape.circle,
-                border: Border.all(color: Colors.white60, width: 2.0),
+                shape: BoxShape.rectangle,
+                border: Border.all(color: Colors.grey.shade300, width: 2.5),
+                borderRadius: const BorderRadius.vertical(
+                    bottom: Radius.circular(34), top: Radius.circular(5)),
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                        // height: double.infinity,
+                        // width: 20,
+                        // color: Colors.blue,
+                        ),
+                  ),
+                  Container(
+                    // height: 50,
+                    width: screenWidth - 20,
+                    // child: customeText(theData: 'fffffff'),
+                    decoration: BoxDecoration(
+                      // color: Colors.cyan,
+                      borderRadius: BorderRadius.circular(22),
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                          Colors.grey.shade300,
+                          Colors.grey.shade200,
+                          Colors.white,
+                        ],
+                      ),
+                    ),
+                    child: TextFormField(
+                      // focusNode: focusNode,
+                      controller: controllerMessage,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.multiline,
+                      textInputAction: TextInputAction.newline,
+                      cursorColor: Colors.cyan,
+                      onSaved: (message) {},
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white70,
+                        hintText: "Message...",
+                        prefixIcon: customePaddingOnly(
+                          thePaddingLeft: 10,
+                          theChild: customeIconShaderMask(
+                            theIcon: Icons.emoji_emotions_outlined,
+                            theSize: 28,
+                          ),
+                        ),
+                        suffixIcon: customePaddingOnly(
+                          thePaddingRight: 10,
+                          theChild: customeIconButton(
+                            theOnPressed: () => controllerMessage.clear(),
+                            theIcon: Icons.close,
+                            theSize: 22,
+                            theColor: Colors.grey.shade400,
+                          ),
+                        ),
+                        border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(40)),
+                            borderSide: BorderSide.none),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
