b77b0d70c7bccdfb32de507bc30af2c022b56f1c
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-09-18 15:14:04 +0300

    deleting useless codes in stars screen

--- lib/src/screens/screens_from_enddrawer/screen_filter_stars.dart
+++ lib/src/screens/screens_from_enddrawer/screen_filter_stars.dart
@@ -1,24 +1,3 @@
-/*import 'package:flutter/material.dart';
-import 'package:grasp_app/src/reusable_codes/functions/functions.dart';
-import 'package:grasp_app/src/reusable_codes/widgets/end_drawer/widget_end_drawer.dart';
-
-class ScreenFilterStars extends StatelessWidget {
-  const ScreenFilterStars({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      endDrawer: SafeArea(
-        child: EndDrawer(),
-      ),
-      appBar: AppBar(
-        leading: functionArrowbackIconButton(context),
-        title: const Text('Stars'),
-      ),
-    );
-  }
-}*/
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
