af70a326975e00edd9c19a15d1ff7a30f3df9d98
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-10-01 23:58:59 +0300

    update WidgetGraspGuidanceExpansionTile

--- lib/src/reusable_codes/widgets/end_drawer/grasp_guidance/widget_grasp_guidance_expansion_tile.dart
+++ lib/src/reusable_codes/widgets/end_drawer/grasp_guidance/widget_grasp_guidance_expansion_tile.dart
@@ -23,44 +23,78 @@ class Widget_GraspGuidanceExpansionTileState
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      tilePadding: EdgeInsets.only(left: 30, right: 15),
-      childrenPadding: const EdgeInsets.only(left: 30, right: 20),
-      expandedAlignment: Alignment.centerLeft,
-      expandedCrossAxisAlignment: CrossAxisAlignment.start,
-      iconColor: Colors.cyan.shade800,
-      // textColor: Colors.white.withOpacity(0.8),
-      textColor: Colors.cyan.shade800,
-      // backgroundColor: Colors.cyan.shade50,
-      collapsedIconColor: Colors.white,
-      collapsedTextColor: Colors.white,
-      title: customeText(
-          theData: '${widget.graspGuidanceId}. ${widget.graspGuidanceTitle}',
-          theFontFamily: "MavenPro",
-          theFontWeight: FontWeight.w600,
-          theFontSize: 17,
-          theLetterSpacing: 0.5),
-      trailing: customeIcon(
-        theIcon:
-            _customTileExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+    return Container(
+      // height: 55.0,
+      margin: const EdgeInsets.only(bottom: 10.0, left: 6.0, right: 6.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(13.0),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(0.0, 1.0), //(x,y)
+            blurRadius: 06.0,
+          ),
+        ],
       ),
-      children: <Widget>[
-        customeText(
-          theData: widget.graspGuidanceDescription,
-          theFontSize: 015,
-          // theFontWeight: FontWeight.w400,
-          theLetterSpacing: 0.4,
-          theColor: Colors.black54,
-          theFontFamily: "MavenPro",
-          // theMaxLines: 1,
+      child: ExpansionTile(
+        // backgroundColor: Colors.green,
+
+
+        tilePadding: EdgeInsets.only(left: 15, right: 15),
+        childrenPadding: const EdgeInsets.only(left: 20, right: 20),
+        expandedAlignment: Alignment.centerLeft,
+        expandedCrossAxisAlignment: CrossAxisAlignment.start,
+        // iconColor: Colors.cyan.shade800,
+        // textColor: Colors.white.withOpacity(0.8),
+        textColor: Colors.black,
+        // backgroundColor: Colors.cyan.shade50,
+        // collapsedIconColor: Colors.black,
+        // collapsedTextColor: Colors.black,
+        title: Row(
+          children: [
+            Container(
+              width: 25,
+              // color: Colors.red,
+              child: customeText(
+                theData: widget.graspGuidanceId,
+                theFontSize: 18,
+                theTextAlign: TextAlign.center,
+              ),
+            ),
+            customeText(
+              theData:_customTileExpanded ? '- ${widget.graspGuidanceTitle}' : '  ${widget.graspGuidanceTitle}',
+              theFontFamily: "MavenPro",
+              theFontWeight: FontWeight.w600,
+              theFontSize: 17,
+              theLetterSpacing: 0.5,
+              // theTextAlign: TextAlign.center,
+            ),
+          ],
         ),
-        const SizedBox(
-          height: 15,
-        )
-      ],
-      onExpansionChanged: (bool expanded) {
-        setState(() => _customTileExpanded = expanded);
-      },
+        trailing: customeIcon(
+          theIcon:
+              _customTileExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+        ),
+        children: <Widget>[
+          customeText(
+            theData: widget.graspGuidanceDescription,
+            theFontSize: 015,
+            // theFontWeight: FontWeight.w400,
+            theLetterSpacing: 0.4,
+            theColor: Colors.black,
+            theFontFamily: "MavenPro",
+            theTextAlign: TextAlign.justify,
+            // theMaxLines: 1,
+          ),
+          const SizedBox(
+            height: 15,
+          )
+        ],
+        onExpansionChanged: (bool expanded) {
+          setState(() => _customTileExpanded = expanded);
+        },
+      ),
     );
   }
 }
