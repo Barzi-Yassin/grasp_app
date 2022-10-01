c588e17df9c67940e6d9e790f69e13db666571eb
Barzy-Yasin <barzy.nettwork@gmail.com>
2022-10-01 22:38:29 +0300

    changing datalistGraspGuidance id to roman numural

--- lib/src/data/datalist_grasp_gidance.dart
+++ lib/src/data/datalist_grasp_gidance.dart
@@ -1,21 +1,21 @@
 const datalistGraspGuidance = [
   {
-    "grasp_guidance_id": "1",
+    "grasp_guidance_id": "i",
     "grasp_guidance_title": "Signin and Signup",
     "grasp_guidance_description": "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
   },
   {
-    "grasp_guidance_id": "2",
+    "grasp_guidance_id": "ii",
     "grasp_guidance_title": "Subjects",
     "grasp_guidance_description": "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
   },
   {
-    "grasp_guidance_id": "3",
+    "grasp_guidance_id": "iii",
     "grasp_guidance_title": "Grasp Files",
     "grasp_guidance_description": "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
   },
   {
-    "grasp_guidance_id": "4",
+    "grasp_guidance_id": "iv",
     "grasp_guidance_title": "Grasp Notes",
     "grasp_guidance_description": "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
   },
